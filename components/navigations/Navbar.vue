c22e0a80389c0522a355b2482c0fe34dad25bb67
fearhunt <firhanmahdi@gmail.com>
2021-01-18 01:01:24 +0700

    maintenance

--- components/navigations/Navbar.vue
+++ components/navigations/Navbar.vue
@@ -1,10 +1,20 @@
 <template>
   <b-navbar class="fixed-top" toggleable="lg" variant="simauto">
-    <b-navbar-nav>
-      <nuxt-link to="/">
-        Simauto
-      </nuxt-link>
-    </b-navbar-nav>
+    <b-navbar-brand>
+      <h3 style="color: white">SimAuto</h3>
+      <!-- <nuxt-link to="/"> -->
+      <!-- </nuxt-link> -->
+    </b-navbar-brand>
+
+    <b-nav-toggle target="nav-menu-collapse"></b-nav-toggle>
+
+    <b-collapse id="nav-menu-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <a href="#" class="simauto-nav-link">Upload</a>
+        <a href="#" class="simauto-nav-link">How It Works</a>
+        <a href="#" class="simauto-nav-link">Meet Our Team</a>
+      </b-navbar-nav>
+    </b-collapse>
   </b-navbar>
 </template>
   
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,11 @@
 <template>
   <div>
-    <section id="introduction">
-      <b-container>
-        <b-row>
-          <b-form @submit="getData">
-            <b-form-file
-              v-model="file_krs"
-              placeholder="Cek KRS"
-              drop-placeholder="Taruh KRS"
-            ></b-form-file>
-
-            <b-button type="submit">Check</b-button>
-          </b-form>
-          
-          <br>
-          
-          <p>
-            {{ datas[0] }}
-          </p>
-
-          <b-form-input v-model="color_picker" type="color"></b-form-input>
-
-          <div id="test"></div>
-        </b-row>
-      </b-container>
+    <section class="d-flex align-items-center" style="height: 100vh; background: #003568">
+      <div class="text-center" style="width: 100%">
+        <h1 id="coming-soon" style="font-size: 5rem; color: #FFB506">
+        COMING SOON
+      </h1>
+      </div>
     </section>
   </div>
 </template>
@@ -34,55 +16,10 @@
   const FormData = require('form-data');
 
   export default {
-    data() {
-      return {
-        file_krs: null,
-        datas: [],
-        color_picker: '#000'
-      }
-    },
 
-    methods: {
-      getData(event) {
-        event.preventDefault();
-
-        this.$nextTick(() => {
-          this.$nuxt.$loading.start()
-        });
-
-        const file_krs_name = this.file_krs.name;
-
-        const data_krs_form = new FormData();
-        data_krs_form.append('file', this.file_krs, file_krs_name);
-        
-        this.$axios({
-          method: 'post',
-          url: 'https://krs-reader.herokuapp.com/post_krs',
-          data: data_krs_form
-        }).then((response) => {
-          this.$nextTick(() => {
-            setTimeout(() => this.$nuxt.$loading.finish(), 500);
-          });
-
-          console.log('Fetch KRS data success')
-          this.datas.push(response.data)
-          document.getElementById('test').style.background = this.color_picker;
-        }).catch((err) => {
-          this.$nextTick(() => {
-            setTimeout(() => this.$nuxt.$loading.finish(), 500);
-          });
-
-          console.error(err)
-        })
-      }
-    }
   }
 </script>
 
 <style>
-  #test {
-    width: 100px;
-    height: 100px;
-    border: 1px solid black;
-  }
+  
 </style>
